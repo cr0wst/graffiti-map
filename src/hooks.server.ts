a0a4878897a8ef10475589e22a53f8b6a9b0a3b5
Steve Crow <steve@smcrow.com>
2023-11-22 13:40:25 -0500

    Change pg import

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
 import { DB_CONNECTION_STRING } from '$env/static/private';
-import { Client } from 'pg';
+
+import pg from 'pg';
+const { Client } = pg;
+
 export const handle: Handle = async ({ event, resolve }) => {
 	const db = new Client({
 		connectionString: DB_CONNECTION_STRING
